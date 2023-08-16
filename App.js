befb2d2397a2339556650104400762c8f038b364
Rocio Jacob <rochijacob@gmail.com>
2023-08-16 11:54:32 -0300

    .

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import io from 'socket.io-client';
 
-export default function App() {
+const App = () => {
+  let socket;
+  const [message, setMessage] = useState('')
+  
+
+  useEffect(() => {
+    socket = io('http://192.168.1.184:5001');
+
+    socket.on('connect', () => {
+      console.log('Connected to the server');
+    });
+
+    socket.on('message', (msg) => {
+      console.log('Received message:', msg.data);
+      setMessage(msg.data)
+    });
+
+
+    return () => {
+      if (socket) {
+        socket.disconnect();
+      }
+    };
+  }, []);
+
+  const sendMessage = () => {
+    if (socket) {
+      console.log('sending message')
+      socket.emit('message', 'Hello from React Native!');
+    } else {
+      console.log('No socket')
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Here we will listen the server</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>{socket ? 'Socket connected' : 'Socket not connected'}</Text>
+      <Text>There is a message for you: {message}</Text>
+      <Text style={{color: 'red'}} onPress={sendMessage}>Send Message</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
